d03a52a536a6aaa310acdbaaf50e60c709a81705
LilYxa <ilia.elland@mail.ru>
2023-02-14 22:08:31 +0300

    Проведена декомпозиция, добавлена постраничная навигация (изучен React router

--- src/App.js
+++ src/App.js
@@ -1,82 +1,21 @@
-import React, {useState, useRef, useMemo, useEffect} from 'react';
-import PostFilter from './components/PostFilter';
-import PostForm from './components/PostForm';
-import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyModal from './components/UI/MyModal/MyModal';
-import { usePosts } from './hooks/usePosts';
+import React from 'react';
 import './styles/App.css';
-import axios from 'axios'
-import PostService from './API/PostService';
-import Loader from './components/UI/Loader/Loader';
-import { useFetching } from './hooks/useFetching';
-import { getPageCount, getPagesArray } from './utils/pages';
-import Pagination from './components/UI/pagination/Pagination';
+import {BrowserRouter, Link, Navigate, Route, Routes} from 'react-router-dom'
+import { About } from './pages/About';
+import Posts from './pages/Posts';
+import { NavBar } from './components/UI/NavBar/NavBar';
+import Error from './pages/Error';
+import AppRouter from './components/AppRouter';
+
 
 function App() {
-    const [posts, setPosts] = useState([])
-    const [filter, setFilter] = useState({sort: '', query: ''})
-    const [modal, setModal] = useState(false)
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-
-    const [totalPages, setTotalPages] = useState(0)
-    const [limit, setLimit] = useState(10)
-    const [page, setPage] = useState(1)
-
-    const [fetchPosts, isPostsLoading,postError] = useFetching(async (limit, page) => {
-      const response = await PostService.getAll(limit, page);
-      setPosts(response.data)
-      const totalCount = response.headers['x-total-count']
-      setTotalPages(getPageCount(totalCount, limit))
-    })
-
-    useEffect(() => {
-        fetchPosts(limit, page)
-    }, [page])
-
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-        setModal(false)
-    }
-
-    const removePost = (post) => {
-      setPosts(posts.filter(p => p.id !== post.id))
-    }
-
-    const changePage = (page) => {
-        setPage(page);
-        fetchPosts(limit, page)
-    }
-
-  return (
-    <div className="App">
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-          Create new post
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-          <PostForm create={createPost}/>
-      </MyModal>
-      <hr style={{margin: '15px 0'}}/>
-      <PostFilter 
-          filter={filter}
-          setFilter={setFilter}
-      />
-          {postError &&
-              <h1>Error ${postError}</h1>
-          }
-          {isPostsLoading
-              ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
-                    <Loader/>
-                </div>
-              : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'List of posts'}/>   
-          }
-          <Pagination 
-            page={page} 
-            changePage={changePage}
-            totalPages={totalPages}
-          />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+          <NavBar/>          
+          <AppRouter/>
+        </BrowserRouter>
+    )    
+  
 }
 
 export default App;
